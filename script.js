6d8c989b83d41216c48a168673a23b613197e554
dharshuarjun <dhanalakshm77@gmail.com>
2022-08-20 18:37:08 +0530

    1

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ div.innerHTML=`
 <h1>Foodish</h1>
 <h3>Enter a food name:(idly,dosa,pizza,burger etc..)</h3>
 <input type="text"id="txt">
-<button type="button" onclick="foo()">Search</button>
+<button type="button" onclick="getData()">Search</button>
 <div id="food"></div>`;
 div.style.textAlign="center";
 document.body.append(div);
 
-async function foo(){
+async function getData(){
     
     let cc=document.getElementById("txt").value;
     let res= await fetch(` https://foodish-api.herokuapp.com/api/images/${cc}`);
